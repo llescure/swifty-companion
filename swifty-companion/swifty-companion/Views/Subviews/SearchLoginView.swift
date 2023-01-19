537727f4268297e80eaccca8d13ec161bb11647a
Lea Lescure <lea.lescure@hotmail.fr>
2023-01-19 09:43:22 +0100

    fix(Loading View): loading view is generated while user date is not loaded

--- swifty-companion/swifty-companion/Views/Subviews/SearchLoginView.swift
+++ swifty-companion/swifty-companion/Views/Subviews/SearchLoginView.swift
@@ -15,28 +15,33 @@ struct SearchLoginView: View {
     
     var body: some View {
         NavigationStack {
-            VStack(spacing: 10) {
-                LoginEntryView(loginSelected: $loginSelected, token: token)
-                Button (action: {
-                    if (!loginSelected.isEmpty) {
-                        Task {
-                            await user.fetchData(token:token, login:loginSelected)
+            if (!user.isDataLoaded) {
+                VStack(spacing: 10) {
+                    LoginEntryView(loginSelected: $loginSelected, token: token)
+                    Button (action: {
+                        if (!loginSelected.isEmpty) {
+                            Task {
+                                await user.fetchData(token:token, login:loginSelected)
+                            }
                         }
+                    }) {
+                        Label("SEARCH", systemImage: "magnifyingglass")
+                            .labelStyle(.buttonIcon)
                     }
-                }) {
-                    Label("SEARCH", systemImage: "magnifyingglass")
-                        .labelStyle(.buttonIcon)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color("MainBlue"))
+                    .padding(.bottom, 60)
+                    .padding(.horizontal, 40)
                 }
-                .buttonStyle(.borderedProminent)
-                .tint(Color("MainBlue"))
-                .padding(.bottom, 60)
-                .padding(.horizontal, 40)
+                .frame(height: 120)
+                .alert("The user you are looking for doesn't exit", isPresented: $user.isNotExisting) {
+                    Button("OK", role:.cancel) {
+                        loginSelected = ""
+                    }
+                }
             }
-            .frame(height: 120)
-            .alert("The user you are looking for doesn't exit", isPresented: $user.isNotExisting) {
-                Button("OK", role:.cancel) {
-                    loginSelected = ""
-                }
+            else {
+                LoadingView()
             }
         }
         .navigationDestination(isPresented: $user.isDataLoaded) {
