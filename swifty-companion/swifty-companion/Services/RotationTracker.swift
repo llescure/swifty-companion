41f38831e86e23128f474cca5f2c7f4416382d59
Lea Lescure <lea.lescure@hotmail.fr>
2023-01-24 11:33:43 +0100

    fix(orientation): fix orientation

---
+++ swifty-companion/swifty-companion/Services/RotationTracker.swift
@@ -0,0 +1,30 @@
+//
+//  RotationTracker.swift
+//  swifty-companion
+//
+//  Created by Léa Lescure on 24/01/2023.
+//
+
+import Foundation
+import SwiftUI
+
+// Our custom view modifier to track rotation and
+// call our action
+struct DeviceRotationViewModifier: ViewModifier {
+    let action: (UIDeviceOrientation) -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+// A View wrapper to make the modifier easier to use
+extension View {
+    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
+    }
+}
